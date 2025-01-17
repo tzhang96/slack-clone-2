a49cb0245a90653390e5c6ed45a32fd5c78e0507
tzhang96 <tzhang96@g.ucla.edu>
2025-01-17 12:51:18 -0500

    Further useMemo fixes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+      mutations: {
+        retry: false,
+      },
+    },
+  }))
 
   return (
     <QueryClientProvider client={queryClient}>
